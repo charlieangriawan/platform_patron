63ede27bd26880d8dd3f6df31f85218aaab705ad
meesiammaihum <leesim.yap.2019@sis.smu.edu.sg>
2021-02-20 21:49:22 +0800

    added css for home,login and registration

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -12,22 +12,23 @@ const Login = (props) => {
   }
   return (
     <div className="container border p-3 mt-5">
-      <p className="fs-3">Login</p>
-      <div className="mb-3">
-        <label for="formGroupExampleInput" className="form-label">Phone</label>
-        <input type="text" className="form-control" id="formGroupExampleInput" placeholder="eg.92836172"
+      <p className="elements-login" style={{ fontWeight: 800, fontSize: '24px', textAlign: 'left'}}> Welcome back</p>
+      <p className="elements-login" style={{fontWeight: 600, fontSize: '16px', fontStyle: 'italic', textAlign: 'left'}}> Sign in to continue</p>
+      <div className="mb-3" align="left">
+        <label for="formGroupExampleInput" className="form-label" style={{ color: "#EE693F", fontWeight: 600, fontSize: '16px', textAlign: 'left'}}>Mobile number</label>
+        <input type="text" className="form-control" id="formGroupExampleInput" placeholder="eg. 92836172"
           onChange={(e)=>setForm({ ...form, phone: e.target.value })}
           value={form.phone}
         />
       </div>
-      <div className="mb-3">
-        <label for="formGroupExampleInput2" className="form-label">Password</label>
-        <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="eg.2u2jbd%Ts./2"
+      <div className="mb-3" align="left">
+        <label for="formGroupExampleInput2" className="form-label" style={{ color: "#EE693F", fontWeight: 600, fontSize: '16px', textAlign: 'left'}}>Password</label>
+        <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="eg. 2u2jbd%Ts./2"
           onChange={(e)=>setForm({ ...form, password: e.target.value })}
           value={form.password}
         />
       </div>
-      <button type="button" className="btn btn-primary btn-lg" onClick={handleLogin}>Login</button>
+      <button type="button" className="btn btn-primary btn-lg btn-login" onClick={handleLogin}>Log in</button>
     </div>
   )
 }
--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -9,24 +9,25 @@ const Registration = (props) => {
     console.log(form)
     props.register(form)
   }
+
   return (
     <div className="container border p-3 mt-5">
-      <p class="fs-3">Registration</p>
-      <div className="mb-3">
-        <label for="formGroupExampleInput" className="form-label">Phone</label>
-        <input type="text" className="form-control" id="formGroupExampleInput" placeholder="eg.92836172"
+      <p class="elements-login"style={{ fontWeight: 800, fontSize: '24px', textAlign: 'left'}}>Create account</p>
+      <div className="mb-3" align="left">
+        <label for="formGroupExampleInput" className="form-label" style={{ color: "#EE693F", fontWeight: 600, fontSize: '16px', textAlign: 'left'}}>Phone</label>
+        <input type="text" className="form-control" id="formGroupExampleInput" placeholder="eg. 92836172"
           onChange={(e)=>setForm({ ...form, phone: e.target.value })}
           value={form.phone}
         />
       </div>
-      <div className="mb-3">
-        <label for="formGroupExampleInput2" className="form-label">Password</label>
-        <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="eg.2u2jbd%Ts./2"
+      <div className="mb-3" align="left">
+        <label for="formGroupExampleInput2" className="form-label" style={{ color: "#EE693F", fontWeight: 600, fontSize: '16px', textAlign: 'left'}}>Password</label>
+        <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="eg. 2u2jbd%Ts./2"
           onChange={(e)=>setForm({ ...form, password: e.target.value })}
           value={form.password}
         />
       </div>
-      <button type="button" class="btn btn-primary btn-lg" onClick={handleLogin}>Registration</button>
+      <button type="button" class="btn btn-primary btn-lg btn-login" onClick={handleLogin}>Sign up</button>
     </div>
   )
 }
